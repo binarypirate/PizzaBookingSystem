69c39dda3a9cc33f73fd7d5b56c7bbfb55123a16
Attaullah Khan <attaullahkhanlodhi@gmail.com>
2024-10-05 10:46:55 +0500

    pizza booking system complete

--- pages/cart.php
+++ pages/cart.php
@@ -1,98 +1,50 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Cart</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-  </head>
-  <body>
-    <div>
-      <div class="bg-orange-400 text-center p-5 text-3xl font-bold">
-        <h1>Pizza Booking System</h1>
-      </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pizza Booking System</title>
+    <link rel="stylesheet" href="/styles/style.css">
+</head>
+<body>
 
-      <div class="bg-orange-300 m-px">
-            <div class="flex flex-row p-4 ml-5 mr-5">
-                <div class="text-xl mr-auto mt-2 font-bold">
-                <h1>Cart</h1>
+<div class="root w-screen">
+
+    <div class="h-32 bg-red-500 flex justify-between px-5 items-center">
+        <p class="text-3xl font-bold uppercase text-white">CART</p>
+        <p class="checkout-button text-3xl font-bold uppercase text-white bg-blue-500 p-2 rounded hover:bg-blue-600 cursor-pointer">Checkout</p>
+    </div>
+
+    <div class="order-items w-screen flex flex-col gap-3 p-3 bg-blue-500">
+        <div class="order-item flex w-full h-72 bg-sky-700 hidden">
+            <div class="weight-0.5 flex flex-col justify-center items-center gap-3">
+                <div class="w-3/4 h-3/4">
+                    <img class="w-full h-full object-cover" src="/images/cheese_pizza.jpg" alt="cheese pizza">
                 </div>
-                <div
-                class="my-button bg-blue-500 px-6 py-2 rounded-full text-white font-bold cursor-pointer hover:bg-blue-600 ml-7">
-                <button>Checkout</button>
-                </div>
+                <p class="text-3xl text-white uppercase font-bold item-title">Cheese Pizza</p>
             </div>
-        </div>
-      
-
-      <div class="flex flex-row mt-1">
-            <div class="ml-5">
-             <img class="h-80 rounded-xl" src="/images/cheese_pizza.jpg" alt="" />
-            </div>
-            <div class="flex flex-col">
-            <div class="ml-10 font-semibold">
-                <p>Cheez Pizza</p>
-            </div>
-
-            <div class="ml-10 mt-10">
-                <p>Pizza booking</p>
-            </div>
-
-            <div class="ml-10">
-                <p>Pizza Booking system</p>
-            </div>
-
-            <div class="ml-10">
-                <p>Pizza Booking System</p>
-            </div>
-            </div>
-      </div>
-
-        <div class="flex flex-row mt-5">
-            <div class="ml-5">
-            <img class="h-80 rounded-xl" src="/images/vegetable_pizza.jpg" alt="" />
-            </div>
-            <div class="flex flex-col">
-                <div class="ml-10 font-semibold">
-                    <p>Vegetable Pizza</p>
+            <div class="weight-2 flex flex-col justify-center">
+                <div class="form-group flex gap-3 p-3 items-center">
+                    <p class="text-3xl text-white bg-blue-500 p-3">Size:</p>
+                    <p class="text-3xl text-white size-container">10$</p>
                 </div>
 
-                <div class="ml-10 mt-10">
-                    <p>Pizza booking</p>
+                <div class="form-group flex gap-3 p-3 items-center">
+                    <p class="text-3xl text-white bg-blue-500 p-3">Quantity:</p>
+                    <p class="text-3xl text-white quantity-container">10$</p>
                 </div>
 
-                <div class="ml-10">
-                    <p>Pizza Booking system</p>
-                </div>
-
-                <div class="ml-10">
-                    <p>Pizza Booking System</p>
-                </div>
-            </div>
-        </div>
-        
-
-        <div class="flex flex-row mt-5">
-            <div class="ml-5">
-                <img class="h-80 rounded-xl" src="/images/fries.jpg" alt="" />
-            </div>
-            <div class="flex flex-col">
-                <div class="ml-10 font-semibold">
-                    <p>Fries</p>
-                </div>
-
-                <div class="ml-10 mt-10">
-                    <p>Pizza booking</p>
-                </div>
-
-                <div class="ml-10">
-                    <p>Pizza Booking system</p>
-                </div>
-
-                <div class="ml-10">
-                    <p>Pizza Booking System</p>
+                <div class="form-group flex gap-3 p-3 items-center">
+                    <p class="text-3xl text-white bg-blue-500 p-3">Price:</p>
+                    <p class="text-3xl text-white price-container">10$</p>
                 </div>
             </div>
         </div>
     </div>
-  </body>
+
+</div>
+
+<script type="application/javascript" src="/scripts/cart.js"></script>
+
+</body>
 </html>
